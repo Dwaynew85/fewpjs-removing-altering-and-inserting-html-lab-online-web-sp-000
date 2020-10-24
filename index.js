e2ec035c6a28737467e7c1386ff63debdd994b98
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-10-24 04:34:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ let header = document.createElement('h1');
 header.id = 'victory';
 document.body.appendChild(header);
 let newHeader = document.querySelector('h1#victory');
+<<<<<<< HEAD
 newHeader.innerHTML = "YOUR-NAME is the champion";
+=======
+>>>>>>> 18a882b133c49e584cd4642668fb5f3ac796f7f3
